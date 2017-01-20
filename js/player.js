a68321b0c983624a0c5392186d3f9cb33e315dd5
Realseppy <real.fan.seppy@hotmail.de>
2017-01-20 17:19:40 +0100

    Player.js aufgeräumt
    Die Player.js wurde neu strukturiert und alle haben Personen haben jetzt
    die Videofunktion
    -seppi

--- js/player.js
+++ js/player.js
@@ -1,29 +1,77 @@
-var vid_meki = document.getElementById('meki');
-var vid_meki_controls = document.getElementById('meki-btn')
-var isPaused = true;
+//PLAYER.js - LAST EDITED 20.01.17----------------------------------------------
+//------------------------------------------------------------------------------
 
-vid_meki.addEventListener('click', function() {
-  if (isPaused){
-    vid_meki.play();
-    vid_meki_controls.className = "video-stop";
-    isPaused = false;
-  }else{
-    vid_meki.pause();
-    vid_meki.currentTime = 0;
-    vid_meki.load();
-    vid_meki_controls.className = "video-play";
-    isPaused = true;
-  }
-}, false);
+//Die Video Objekte:
+//Name soll ID entsprechen und ControlName der ButtonID
+var Video_Meki = {
+	name: "meki",
+	name_control: "meki-btn",
+	isPaused: true
+};
+var Video_Seppi = {
+	name: "seppi",
+	name_control: "seppi-btn",
+	isPaused: true
+};
+var Video_Uta = {
+	name: "uta",
+	name_control: "uta-btn",
+	isPaused: true
+};
+var Video_Andre = {
+	name: "andre",
+	name_control: "andre-btn",
+	isPaused: true
+};
 
+//Liste mit VideoObjekten
+var VideoList = Array(Video_Meki, Video_Seppi, Video_Uta, Video_Andre);
+//Funktion die einem VideoObjekt verschiedene Events (Click,mOver,mOut) verteilt
+function AddEvents(VideoObjekt) {
+	//ClickEvent
+	document.getElementById(VideoObjekt.name).addEventListener('click', function() {
+		click(this, VideoObjekt);
+	}, false);
+	//MouserOverEvent
+	document.getElementById(VideoObjekt.name_control).addEventListener('mouseover', function() {
+		mouseover(this, VideoObjekt);
+	});
+	//MouseOutEvent
+	document.getElementById(VideoObjekt.name_control).addEventListener('mouseout', function() {
+		mouseout(this, VideoObjekt);
+	});
+}
 
-vid_meki_controls.addEventListener('mouseover', function(){
-  if(!isPaused){
-    vid_meki_controls.className = "video-stop";
-  }
-});
-vid_meki_controls.addEventListener('mouseout', function(){
-  if(!isPaused){
-    vid_meki_controls.className = "video-stop hide";
-  }
-});
+//Alle Videos aus der Liste bekommen Events
+for (var i = 0; i < VideoList.length; i++) AddEvents(VideoList[i]);
+
+//ClickFunktion
+function click(Video, VideoObjekt) {
+  //Button wird aus dem DOM geholt
+	domControls = document.getElementById(Video.id + "-btn");
+	if (VideoObjekt.isPaused) {  //Wenn pausiert
+    //Video abspielen
+		Video.play();
+		domControls.className = "video-stop";
+		VideoObjekt.isPaused = false;
+	} else { //Bereits gestartet
+    //Video pausieren
+		Video.pause();
+		Video.currentTime = 0;
+		Video.load();
+		domControls.className = "video-play";
+		VideoObjekt.isPaused = true;
+	}
+}
+//MouseOver Funktion
+function mouseover(VideoControl, VideoObjekt) {
+	if (!VideoObjekt.isPaused) {
+		VideoControl.className = "video-stop";
+	}
+}
+//MouseOut Funktion
+function mouseout(VideoControl, VideoObjekt) {
+	if (!VideoObjekt.isPaused) {
+		VideoControl.className = "video-stop hide";
+	}
+}
