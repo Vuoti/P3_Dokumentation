f9b5fc9f68b4fc5d39aa0d305dbe9eb8f3ae19a4
MacCharoni <mlam@macmini.local>
2017-01-28 15:44:04 +0100

    Quellen raus

--- Quellen/Duden | Nach­hal­tig­keit | Rechtschreibung, Bedeutung, Definition-Dateien/praeludium_duden.js
+++
@@ -1,3 +0,0 @@
-if (document.readyState !== 'complete' && document.readyState !== 'interactive'){
-    document.write('<script type="text/javascript" src="//cdn.stroeerdigitalgroup.de/metatag/live/duden.de/metaTag.min.js"><\/script>');
-}
